ce300cd01227bbf221d0c4929f5b623dd09df302
Bruno Wosiak <brunofwosiak@gmail.com>
2020-05-05 18:53:58 -0300

    first commit

---
+++ src/main/java/com/ufpr/es/divresidapi/DivresidApiApplication.java
@@ -0,0 +1,13 @@
+package com.ufpr.es.divresidapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivresidApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivresidApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufpr/es/divresidapi/DivresidApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpr.es.divresidapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivresidApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
