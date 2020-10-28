7b9058ac5793b71e85cfeb04de4ef0bc8da81245
wosiakkk <brunofwosiak@gmail.com>
2020-10-27 21:31:42 -0300

    Adicionado primeira versao da classe para divisao de despesa coletiva

--- src/main/java/com/ufpr/es/divresidapi/dto/CollectiveEntryDTO.java
+++ src/main/java/com/ufpr/es/divresidapi/dto/CollectiveEntryDTO.java
@@ -1,61 +1,78 @@
 package com.ufpr.es.divresidapi.dto;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import com.ufpr.es.divresidapi.model.Category;
 import com.ufpr.es.divresidapi.model.Entry;
 import com.ufpr.es.divresidapi.model.Property;
 import com.ufpr.es.divresidapi.model.User;
 
 public class CollectiveEntryDTO {
 	
-	private Long id;
-	
-	private double amount;
-	
+	private String name;
+	private String description;
+	private String type;
+	private float amount;
+	private LocalDate date;
+	private Category category;
 	private Property property;
-	
+	private List<User> residents;
 	private User user;
 	
-	private List<Entry> generatedEntries;
-
-	public Long getId() {
-		return id;
+	public String getName() {
+		return name;
 	}
-
-	public void setId(Long id) {
-		this.id = id;
+	public void setName(String name) {
+		this.name = name;
 	}
-
-	public double getAmount() {
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public float getAmount() {
 		return amount;
 	}
-
-	public void setAmount(double amount) {
+	public void setAmount(float amount) {
 		this.amount = amount;
 	}
-
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
 	public Property getProperty() {
 		return property;
 	}
-
 	public void setProperty(Property property) {
 		this.property = property;
 	}
-
+	public List<User> getResidents() {
+		return residents;
+	}
+	public void setResidents(List<User> residents) {
+		this.residents = residents;
+	}
 	public User getUser() {
 		return user;
 	}
-
 	public void setUser(User user) {
 		this.user = user;
-	}
-
-	public List<Entry> getGeneratedEntries() {
-		return generatedEntries;
-	}
-
-	public void setGeneratedEntries(List<Entry> generatedEntries) {
-		this.generatedEntries = generatedEntries;
 	}
 	
 }
---
+++ src/main/java/com/ufpr/es/divresidapi/utils/entry/AmountDivider.java
@@ -0,0 +1,42 @@
+
+package com.ufpr.es.divresidapi.utils.entry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ufpr.es.divresidapi.dto.CollectiveEntryDTO;
+import com.ufpr.es.divresidapi.model.Category;
+import com.ufpr.es.divresidapi.model.Entry;
+import com.ufpr.es.divresidapi.model.Property;
+import com.ufpr.es.divresidapi.model.User;
+
+public class AmountDivider {
+	
+	public List<Entry> prepare(CollectiveEntryDTO dto) {
+		float valor = divide(dto.getAmount(), dto.getResidents().size());
+		return entriesFactory(valor, dto);
+	}
+	
+	private float divide(float value, int numberOfPersons) {
+		return value / numberOfPersons;
+	}
+	
+	private List<Entry> entriesFactory(float individualValue,
+			CollectiveEntryDTO dto ){
+		List<Entry> entries =  new ArrayList<Entry>();
+		for (User user : dto.getResidents()) {
+			Entry e = new Entry();
+			e.setName(dto.getName());
+			e.setDescription(dto.getDescription());
+			e.setType(dto.getType());
+			e.setAmount(individualValue);
+			e.setDate(dto.getDate());
+			e.setPaid(false);
+			e.setCategory(dto.getCategory());
+			e.setUser(dto.getUser());
+			entries.add(e);
+		}
+		return entries;
+	}
+	
+}
