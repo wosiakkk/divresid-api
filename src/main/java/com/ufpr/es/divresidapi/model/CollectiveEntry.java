3cb6f91bfecae1c215702a14ed70d95f1f7a9f09
wosiakkk <brunofwosiak@gmail.com>
2020-11-17 23:04:31 -0300

    AJuste em BD e na entidade de entrada coletiva.
    Iniciado adaptação para estrutura de entrada coletiva

--- src/main/java/com/ufpr/es/divresidapi/model/CollectiveEntry.java
+++ src/main/java/com/ufpr/es/divresidapi/model/CollectiveEntry.java
@@ -1,18 +1,59 @@
 package com.ufpr.es.divresidapi.model;
 
+import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.List;
 
-public class CollectiveEntry {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "collectives")
+public class CollectiveEntry implements Serializable{
+
+	private static final long serialVersionUID = -2895455543687787013L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+	@Column
 	private double amount;
-	
+	@ManyToOne
+	@JoinColumn(name = "property")
 	private Property property;
-	
+	@ManyToOne
+	@JoinColumn(name = "user")
 	private User user;
-	
+	@Column
+	private LocalDate date;
+	@Column
+	private String description;
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "collective_entries",
+			   joinColumns = @JoinColumn(name = "collective_id"),
+			   inverseJoinColumns = @JoinColumn(name = "entry_id"))
 	private List<Entry> generatedEntries;
+	
+	
+	public CollectiveEntry() {}
+
+	public CollectiveEntry(Long id, double amount, Property property, User user, List<Entry> generatedEntries) {
+		super();
+		this.id = id;
+		this.amount = amount;
+		this.property = property;
+		this.user = user;
+		this.generatedEntries = generatedEntries;
+	}
 
 	public Long getId() {
 		return id;
@@ -45,6 +86,22 @@ public class CollectiveEntry {
 	public void setUser(User user) {
 		this.user = user;
 	}
+	
+	public LocalDate getDate() {
+		return date;
+	}
+	
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 	public List<Entry> getGeneratedEntries() {
 		return generatedEntries;
@@ -52,6 +109,31 @@ public class CollectiveEntry {
 
 	public void setGeneratedEntries(List<Entry> generatedEntries) {
 		this.generatedEntries = generatedEntries;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CollectiveEntry other = (CollectiveEntry) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 }
